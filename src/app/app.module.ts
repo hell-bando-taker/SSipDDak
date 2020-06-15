257e2826ec08b7317c10540805655727d3846bfd
mari.kim <im.not.food.mari@gmail.com>
2020-06-15 23:41:48 +0900

    layout init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HudComponent } from './hud/hud.component';
+import { BackgroundComponent } from './background/background.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HudComponent,
+    BackgroundComponent
   ],
   imports: [
     BrowserModule,
